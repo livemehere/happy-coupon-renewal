9016917a73965f9d0885176494cbc29f9a5da17e
kongtaemin <rhdxoals@gmail.com>
2022-01-25 00:11:47 +0900

    2차피드백에서 5만원 이하는 바로결제기능추가

--- app/src/main/java/kr/co/pointmobile/msrdemo/MoneyActivity.java
+++ app/src/main/java/kr/co/pointmobile/msrdemo/MoneyActivity.java
@@ -2,13 +2,34 @@ package kr.co.pointmobile.msrdemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.icu.text.DecimalFormat;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import device.sdk.print.Printer;
+import device.sdk.print.ReceiptPrint;
+import device.sdk.print.utils.ExFormat;
+import kr.co.pointmobile.msrdemo.models.CardAuthResult;
+import kr.co.pointmobile.msrdemo.retrofit.RetrofitFactory;
+import kr.co.pointmobile.msrdemo.retrofit.RetrofitService;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+import static device.sdk.print.Printer.BMP_PRINT_COMMON_SUCCESS;
+import static device.sdk.print.Printer.ERROR_NO_FONT_LIBRARY;
+import static device.sdk.print.ReceiptPrint.ALIGN_CENTER;
+import static device.sdk.print.ReceiptPrint.PRESET_W20H100;
+import static device.sdk.print.ReceiptPrint.PRESET_W30H100;
 
 
 public class MoneyActivity extends AppCompatActivity {
@@ -20,6 +41,67 @@ public class MoneyActivity extends AppCompatActivity {
     private int counter;
     private DecimalFormat myFormatter;
     private String moneyFormat;
+
+
+    //    print
+    private String printBtn;
+    private String grayPlusBtn;
+    private String grayMinusBtn;
+    private String grayValText ="3";
+    private boolean checkBoxBoldStyle;
+
+    private Button textMethodBtn;
+    private Button presetBtn;
+    private Button receiptBtn;
+    private Button resetBtn;
+
+    //    private ScrreceiptollView scrollView;
+    private ImageView imageView;
+
+    private ProgressDialog inprogressDialog;
+
+    private ReceiptPrint receipt;
+    private Printer printer;
+
+    private final int SELECT_TEXT_METHOD = 0;
+    private final int SELECT_PRESET = 1;
+    private final int SELECT_RECEIPT = 2;
+    private final int SELECT_RESET = 3;
+
+
+    public String res_serial = "";
+    public String res_card_no = "";
+    public String res_expire_date = "";
+    public String res_install_period = "";
+    public String res_tot_amt = "";
+    public String res_agentid = "";
+    public String res_onfftid = "";
+    public String res_cert_type = "";
+    public String res_card_user_type = "";
+    public String res_user_nm = "";
+    public String res_user_phone2 = "";
+    public String res_order_no = "";
+    public String res_transeq = "";
+    public String res_result_cd = "";
+    public String res_result_msg = "";
+    public String res_app_date = "";
+    public String res_app_dt = "";
+    public String res_app_tm = "";
+    public String res_auth_no = "";
+    public String res_issuer_nm = "";
+    public String res_payment = "";
+    public String res_regdate = "";
+    public String res_regtime = "";
+    public String res_fee = "";
+    public String res_coupon_no = "";
+
+    private String name;
+    private String companyid;
+    private String ceoname;
+    private String phone;
+    private String address;
+
+
 
 
     @Override
@@ -47,6 +129,17 @@ public class MoneyActivity extends AppCompatActivity {
             expire_date = extras.getString("expire_date");
         }
 
+        SharedPreferences sharedPreferences = getSharedPreferences("marget", Context.MODE_PRIVATE);
+
+        name = sharedPreferences.getString("name","");
+        companyid = sharedPreferences.getString("companyid","");
+        ceoname = sharedPreferences.getString("ceoname","");
+        phone = sharedPreferences.getString("phone","");
+        address = sharedPreferences.getString("address","");
+
+
+        receipt = ((BaseApplication)getApplication()).getReceiptPrint();
+        printer = ((BaseApplication)getApplication()).getPrinter();
 
 
     }
@@ -86,12 +179,90 @@ public class MoneyActivity extends AppCompatActivity {
             //확인을 누를 경우
 
             if(totalStr.length()>0){
-                Intent intent = new Intent(v.getContext(), HalbuActivity.class);
-                intent.putExtra("serial",serial);
-                intent.putExtra("card_no",card_no);
-                intent.putExtra("expire_date",expire_date);
-                intent.putExtra("tot_amt",totalStr);
-                startActivity(intent);
+
+                if(Integer.parseInt(totalStr) < 50000){
+                    RetrofitService networkService = RetrofitFactory.create();
+                    networkService.authCard(serial,card_no,expire_date,"00",totalStr).enqueue(new Callback<CardAuthResult>() {
+                        @Override
+                        public void onResponse(Call<CardAuthResult> call, Response<CardAuthResult> response) {
+                            //  정상 통신 된 경우
+                            if(response.isSuccessful()){
+                                Log.d("카드승인 결과",response.body().result_cd);
+                                if(response.body().result_cd .equals("0000")){
+                                    // 카드 승인이 정상적으로 이루어진 경우
+                                    // TODO: 여기서 영수증 출력하면 됨 (res_ 는 리턴값들)
+                                    res_serial = response.body().serial;
+                                    res_card_no = response.body().card_no;
+                                    res_expire_date = response.body().expire_date;
+                                    res_install_period = response.body().install_period;
+                                    res_tot_amt = response.body().tot_amt;
+                                    res_agentid = response.body().agentid;
+                                    res_onfftid = response.body().onfftid;
+                                    res_cert_type = response.body().cert_type;
+                                    res_card_user_type = response.body().card_user_type;
+                                    res_user_nm = response.body().user_nm;
+                                    res_user_phone2 = response.body().user_phone2;
+                                    res_order_no = response.body().order_no;
+                                    res_transeq = response.body().transeq;
+                                    res_result_cd = response.body().result_cd;
+                                    res_result_msg = response.body().result_msg;
+                                    res_app_date = response.body().app_date;
+                                    res_app_dt = response.body().app_dt;
+                                    res_app_tm = response.body().app_tm;
+                                    res_auth_no = response.body().auth_no;
+                                    res_issuer_nm = response.body().issuer_nm;
+                                    res_payment = response.body().payment;
+                                    res_regdate = response.body().regdate;
+                                    res_regtime = response.body().regtime;
+                                    res_fee = response.body().fee;
+                                    res_coupon_no = response.body().coupon_no;
+
+                                    if(res_install_period.equals("00")){
+                                        res_install_period = "일시불";
+                                    }
+
+                                    Log.d("결제완료된 쿠폰번호",res_coupon_no);
+                                    makeReceipt();
+                                    if (receipt != null) {
+                                        int ret = printer.setGrayValue(3);
+                                        if (ret == BMP_PRINT_COMMON_SUCCESS || ret == ERROR_NO_FONT_LIBRARY) {
+                                            ret = printer.print(receipt);
+                                        }
+
+                                    }
+                                    Intent intent = new Intent(v.getContext(),MainActivity.class);
+                                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                                    startActivity(intent);
+
+
+                                }else{
+                                    // TODO: 카드 승인이 실패한 경우
+                                    Toast.makeText(getApplicationContext(), response.body().result_msg, Toast.LENGTH_SHORT).show();
+                                }
+
+                            }}
+
+                        @Override
+                        public void onFailure(Call<CardAuthResult> call, Throwable t) {
+//                                Toast.makeText(getApplicationContext(), call.toString(), Toast.LENGTH_SHORT).show();
+
+                            Toast.makeText(getApplicationContext(), call.request().toString(), Toast.LENGTH_SHORT).show();
+
+                        }
+                    });
+
+                }else{
+                    Intent intent = new Intent(v.getContext(), HalbuActivity.class);
+                    intent.putExtra("serial",serial);
+                    intent.putExtra("card_no",card_no);
+                    intent.putExtra("expire_date",expire_date);
+                    intent.putExtra("tot_amt",totalStr);
+                    startActivity(intent);
+                }
+
+                //조건 end
+
+
             }else{
 //                Toast.makeText(getApplicationContext(), "가격을 적어주세요", Toast.LENGTH_SHORT).show();
             }
@@ -114,5 +285,57 @@ public class MoneyActivity extends AppCompatActivity {
     public void finishApp(View v){
         finishAffinity();
     }
+
+
+    private void makeReceipt() {
+        try {
+            receipt.setPreset(PRESET_W20H100);
+            receipt.addTextAlign("신용승인\n", ALIGN_CENTER);
+            receipt.setPreset(PRESET_W30H100);
+            receipt.addText(ExFormat.format("%-15s%15s\n", "거 래 일 시 :", res_app_date));
+            receipt.addText(ExFormat.format("%-13s%17s\n", "카 드 번 호 :", res_card_no));
+            receipt.addText(ExFormat.format("%-15s%15s\n", "카 드 종 류 :", res_issuer_nm));
+            receipt.addText(ExFormat.format("%-15s%15s\n", "유 효 기 간 :", "**/**"));
+            receipt.addText(ExFormat.format("%-15s%15s\n", "거 래 유 형 :", "신용승인")); //FIXME
+            receipt.addText(ExFormat.format("%-14s%16s\n", "할 부 개 월 :", res_install_period));
+            receipt.addTextLine("------------------------------");
+            receipt.addText(ExFormat.format("%-15s%13s원\n", "공 급 가 액 :", res_tot_amt)); //FIXME
+            receipt.addText(ExFormat.format("%-15s%15s\n", "부  가  세  :", "0원")); //FIXME
+            receipt.addText(ExFormat.format("%-15s%13s원\n", "합       계 :", res_tot_amt));
+            receipt.addTextLine("------------------------------");
+            receipt.addText(ExFormat.format("%-15s%15s\n", "승 인 번 호 :", res_auth_no));
+            receipt.addTextLine("------------------------------");
+            receipt.addText(ExFormat.format("%-15s%15s\n", "가 맹 점 명 :", name)); //FIXME
+            receipt.addText(ExFormat.format("%-15s%15s\n", "대 표 자 명 :", ceoname)); //FIXME
+            receipt.addText(ExFormat.format("%-15s%15s\n", "사 업 자 NO :", companyid)); //FIXME
+            receipt.addText(ExFormat.format("%-15s%15s\n", "문 의 전 화 :", phone)); //FIXME
+            receipt.addText(ExFormat.format("%s\n\n", address)); //FIXME
+//            receipt.addText("경기 머머시 머머구 머머로00번 길 00(머머동) 0층\n",); //FIXME
+            receipt.addText("[결제대행사]\n");
+            receipt.addText("(주)온오프코리아\n");
+            receipt.addText(ExFormat.format("%s %s\n\n", "사업자번호  :", "636-88-00753"));
+            receipt.addText("[쿠폰발행사]\n");
+            receipt.addText("(주)해피페이\n");
+            receipt.addText(ExFormat.format("%s %s\n", "사업자번호  :", "140-81-49182"));
+            receipt.addText(ExFormat.format("%s %s\n\n\n", "문 의 전 화 :", "1600-8952"));
+            receipt.addText(ExFormat.format("%-15s%15s\n", "", "구매상품내역"));
+            receipt.addTextLine("------------------------------");
+            receipt.addTextAlign("HAPPY COUPON\n", ALIGN_CENTER);
+            receipt.addTextLine("------------------------------");
+            receipt.addText(ExFormat.format("%-15s%13s원\n", "쿠 폰 금 액 :", res_tot_amt));
+            receipt.addText(ExFormat.format("%-15s%15s\n", "쿠 폰 번 호  :", res_coupon_no));
+            receipt.addTextLine("------------------------------");
+            receipt.addText("http://m.happycoupon.co.kr\n");
+            receipt.addText("에서 사용하신 쿠폰번호로 \n");
+            receipt.addText("해피캐시를 적립하신 후\n");
+            receipt.addText("리커버샵(www.recovershop.co.kr\n");
+            receipt.addText("에서 편리하게 사용하세요\n");
+            receipt.addText("(서명/SIGNATURE)\n");
+
+        } catch (ReceiptPrint.BitmapOutOfRangeException e) {
+            e.printStackTrace();
+        }
+    }
+
 
 }
--- app/src/main/java/kr/co/pointmobile/msrdemo/retrofit/RetrofitFactory.java
+++ app/src/main/java/kr/co/pointmobile/msrdemo/retrofit/RetrofitFactory.java
@@ -7,8 +7,8 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitFactory {
-    private static String BASE_URL = "http://192.168.1.32:3000/";
-//    private static String BASE_URL = "http://hc.happycoupon.co.kr/";
+//    private static String BASE_URL = "http://192.168.1.32:3000/";
+    private static String BASE_URL = "http://hc.happycoupon.co.kr/";
 
     public static RetrofitService create(){
 
